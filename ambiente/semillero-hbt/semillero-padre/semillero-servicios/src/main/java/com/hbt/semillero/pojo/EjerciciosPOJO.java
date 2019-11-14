646e76ad867f373315a042e5a960f335fd0c6dfb
yulianaboyaca04 <mary.boyaca@uptc.edu.co>
2019-11-13 19:29:50 -0500

    ejercicios

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/pojo/EjerciciosPOJO.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/pojo/EjerciciosPOJO.java
@@ -11,6 +11,26 @@ package com.hbt.semillero.pojo;
  */
 public class EjerciciosPOJO {
 
-	
+
+	/**
+	 * 
+	 * Metodo encargado de averiguar si un numero es primo o no 
+	 * <b>Caso de Uso</b>
+	 * @author Yuliana
+	 * 
+	 * @param numero
+	 * @return
+	 */
+	public static boolean esPrimo(int numero){
+		  int contador = 2;
+		  boolean primo=true;
+		  while ((primo) && (contador!=numero)){
+		    if (numero % contador == 0) {
+		      primo = false;
+		    }
+		    contador++;
+		  }
+		  return primo;
+	}
 	
 }
---
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjerciciosTest.java
@@ -0,0 +1,65 @@
+/**
+ * EjerciciosMaratonTest.java
+ */
+package com.hbt.semillero.servicios;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.hbt.semillero.pojo.EjerciciosPOJO;
+
+/**
+ * <b>Descripción:<b> Clase que determina
+ * <b>Caso de Uso:<b> 
+ * @author Yuliana
+ * @version 
+ */
+public class EjerciciosTest {
+
+	
+	private String brand;
+	private boolean empty;
+	/**
+	 * 
+	 * Metodo encargado de  probar el ejercicio 2
+	 * <b>Caso de Uso</b>
+	 * @author Yuliana
+	 *
+	 */
+	@Test
+	public  void ejercicio2() {
+		System.out.print("Empty = " + empty);
+		System.out.print(", Brand = " + brand);
+		//La clase no compila con esta linea 	WaterBottle wb = new WatterBottle();
+		//las salidas son: Empty = true, Brand = 
+	}
+	
+	/**
+	 * 
+	 * Metodo encargado de probar el metodo de que si es primo o no
+	 * <b>Caso de Uso</b>
+	 * @author Yuliana
+	 *
+	 */
+	@Test
+	public void ejercicio3() {
+		int numero1 = 5;
+		int numero2 = 222;
+		int numero3 = 0;
+		Assert.assertEquals( EjerciciosPOJO.esPrimo(numero1), true); //5 es primo
+		Assert.assertEquals( EjerciciosPOJO.esPrimo(numero2), false); //222 no es primo
+		Assert.assertEquals(EjerciciosPOJO.esPrimo(numero3), false ); //0 no es primo
+	}
+	
+	/**
+	 * 
+	 * Metodo encargado de probar el metodo 
+	 * <b>Caso de Uso</b>
+	 * @author Yuliana
+	 *
+	 */
+	@Test
+	public void ejercicio4() {
+		
+	}
+}
